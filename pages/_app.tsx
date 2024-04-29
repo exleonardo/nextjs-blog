0ff9979386cad2e49a6655c18e503df375b34b45
Alexander Khomenok <exleonardo@mail.ru>
2024-04-29 13:34:36 +0300

    feat: update type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
+import { ReactElement, ReactNode } from 'react'
+
+import { NextPage } from 'next'
+import { AppProps } from 'next/app'
+
 import '../styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout<P = {}> = {
+  getLayout?: (page: ReactElement) => ReactNode
+} & NextPage<P>
+type AppPropsWithLayout = {
+  Component: NextPageWithLayout
+} & AppProps
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? (page => page)
+
+  return getLayout(<Component {...pageProps} />)
 }
 
 export default MyApp
