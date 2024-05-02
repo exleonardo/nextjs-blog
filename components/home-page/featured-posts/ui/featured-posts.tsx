938ca42432e1676d259c82d55e32f57850cfe322
Alexander Khomenok <exleonardo@mail.ru>
2024-05-02 10:17:18 +0300

    feat: update featured-posts.tsx

--- components/home-page/featured-posts/ui/featured-posts.tsx
+++ components/home-page/featured-posts/ui/featured-posts.tsx
@@ -1,3 +1,16 @@
-export const FeaturedPosts = () => {
-  return <div></div>
+import { PostItemProps } from '@/components/posts/post-item'
+import { PostsGrid, PostsGridProps } from '@/components/posts/posts-grid'
+
+import s from '../style/featured-posts.module.scss'
+
+type FeaturedPostsProps = {
+  posts: PostItemProps[]
+}
+export const FeaturedPosts = ({ posts }: FeaturedPostsProps) => {
+  return (
+    <section className={s.latest}>
+      <h2>Featured Posts</h2>
+      <PostsGrid posts={posts} />
+    </section>
+  )
 }
