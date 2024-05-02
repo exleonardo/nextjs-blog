d4897b6a5bedcf5e1ab68e41cb4eb34713f794f2
Alexander Khomenok <exleonardo@mail.ru>
2024-05-02 10:19:02 +0300

    feat: update posts

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
 
-const AllPostsPage = () => {
-  return <div></div>
+import { getLayout } from '@/components/layout/base-layout/base-layout'
+import { AllPosts } from '@/components/posts/all-posts'
+import { PostItemProps } from '@/components/posts/post-item'
+import { getAllPosts } from '@/helpers/posts-util'
+import { GetStaticProps } from 'next'
+
+export const getStaticProps: GetStaticProps = () => {
+  const allPosts = getAllPosts()
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+  }
 }
 
+type AllPostsPageProps = {
+  posts: PostItemProps[]
+}
+const AllPostsPage = ({ posts }: AllPostsPageProps) => {
+  return <AllPosts posts={posts} />
+}
+
+AllPostsPage.getLayout = getLayout
 export default AllPostsPage
