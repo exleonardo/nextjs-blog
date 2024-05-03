be51060b84dbc7c8ad1de182e2d1671b142d7f63
Alexander Khomenok <exleonardo@mail.ru>
2024-05-03 15:21:54 +0300

    feat: connect mongoDB

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,6 +1,13 @@
+import { MongoClient, ObjectId } from 'mongodb'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+type NewMessageType = {
+  email: string
+  id?: ObjectId
+  message: string
+  name: string
+}
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { email, message, name } = req.body
 
@@ -9,12 +16,36 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
       return
     }
-    const newMessage = {
+    const newMessage: NewMessageType = {
       email,
       message,
       name,
     }
+    let client
 
+    try {
+      client = await MongoClient.connect(
+        'mongodb+srv://exleonardo:1239643k@cluster0.yfysi3d.mongodb.net/my-blog?retryWrites=true&w=majority&appName=Cluster0'
+      )
+    } catch (e) {
+      res.status(500).json({ message: 'Could not connect to database.' })
+
+      return
+    }
+    const db = client.db('my-blog')
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage)
+
+      newMessage.id = result.insertedId
+    } catch (e) {
+      client.close()
+      res.status(500).json({ message: 'Storing message failed' })
+
+      return
+    }
+
+    client.close()
     res.status(201).json({ message: 'Successfully stored message!', messages: newMessage })
   }
 }
